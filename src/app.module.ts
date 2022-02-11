8d888e5a3b9a464c71c45193d5ca7a07581c8b8e
THE HUNG <52951651+JamesHust@users.noreply.github.com>
2022-02-11 11:52:34 +0700

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     BotsModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb://admin:hQGkgHBXj3Agell195H9JxsquUIfPgqh@10.36.6.65:27017/rasa-auth?authSource=admin&readPreference=primary',
+      `mongodb://${process.env.MONGO_RASA_USER}:${process.env.MONGO_RASA_PASSWORD}@${process.env.MONGO_RASA_HOST}:${process.env.MONGO_RASA_POST}/${process.env.MONGO_RASA_DB}?authSource=admin&readPreference=primary`,
     ),
   ],
   controllers: [AppController],
